97654023596d14725ec591102b7de7a30ddd9321
hechtgal <ghatsor@gmail.com>
2024-03-13 21:43:45 +0200

    adjusted the tavily api, doesnt work 100%

---
+++ main.py
@@ -0,0 +1,14 @@
+from tavily import TavilyClient
+
+
+
+
+
+
+if __name__ == '__main__':
+    tavily = TavilyClient(api_key="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")
+    response = tavily.search(query="Should I invest in Tesla today?")
+
+    context = [{"url": obj["url"], "content": obj["content"]} for obj in response.results]
+    what = 0
+
