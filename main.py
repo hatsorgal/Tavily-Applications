39714d6a88172dd920ae21e6cabf181ea60addf5
hechtgal <ghatsor@gmail.com>
2024-03-14 19:31:25 +0200

    adjusted to python 3.11.8

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
     tavily = TavilyClient(api_key="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")
     response = tavily.search(query="Should I invest in Tesla today?")
 
-    context = [{"url": obj["url"], "content": obj["content"]} for obj in response.results]
+    context = [{"url": obj["url"], "content": obj["content"]} for obj in response["results"]]
     what = 0
 
